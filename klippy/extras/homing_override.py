7e458a43f11b954a344dd0f78ea4f9153052e03e
jasonhillier <jason@hillier.us>
2025-06-29 20:09:18 -0600

    Update homing_override.py

--- klippy/extras/homing_override.py
+++ klippy/extras/homing_override.py
@@ -51,7 +51,7 @@ class HomingOverride:
         for axis, loc in enumerate(self.start_pos):
             if loc is not None:
                 pos[axis] = loc
-                homing_axes += "xyz"[axis]
+                homing_axes += "xyzabc"[axis]
         toolhead.set_position(pos, homing_axes=homing_axes)
         # Perform homing
         context = self.template.create_template_context()
